856c9f184f3ca4475398d9f444c34e48b4535641
Lasse Lang <lang.lasse@gmx.de>
2022-08-18 12:24:42 +0200

    Added taking images and playing video

--- WindowManager.py
+++ WindowManager.py
@@ -6,8 +6,57 @@ textColor = "white"
 
 def mainWindow():
   layout = [
-    [sg.Image("", key="-FRAME-"), sg.Image("", key="-CAMERAVIEW-")],
-    [sg.Button("Take image", key="-TAKEIMAGE-", button_color=(textColor, elementColor))]
+    [
+      sg.Listbox(
+        [],
+        size=(
+          5,
+          40
+        ),
+        expand_y=True,
+        background_color=elementColor,
+        text_color=textColor,
+        sbar_background_color=backgroundColor,
+        sbar_arrow_width=0,
+        sbar_trough_color=backgroundColor,
+        key="-FRAMES-",
+        enable_events=True
+      ),
+      sg.Image(
+        "",
+        key="-FRAME-"
+      ),
+      sg.Image(
+        "",
+        key="-CAMERAVIEW-"
+      )
+    ],
+    [
+      sg.Button(
+        "Play",
+        key="-PLAY-",
+        button_color=(
+          textColor,
+          elementColor
+        )
+      ),
+      sg.In(
+        60,
+        key="-FPS-",
+        background_color=elementColor,
+        text_color=textColor
+      )
+    ],
+    [
+      sg.Button(
+        "Take image",
+        key="-TAKEIMAGE-",
+        button_color=(
+          textColor,
+          elementColor
+        )
+      )
+    ]
   ]
   return sg.Window(
     "Stop motion software",
--- main.py
+++ main.py
@@ -5,19 +5,47 @@ import WindowManager
 
 cam = cv2.VideoCapture(0)
 
-fps = 1 / 60
+nextFrame = 0
+frames = []
+currentFrame = 0
+playing = False
 
 mainWindow = WindowManager.mainWindow()
+event, values = mainWindow.read(timeout=1)
 
 while True:
-  event, values = mainWindow.read(timeout=fps)
-  if (event == sg.WIN_CLOSED):
-    break
   ret, frame = cam.read()
   imgbytes=cv2.imencode('.png', frame)[1].tobytes()
   mainWindow["-CAMERAVIEW-"].update(data=imgbytes, subsample=frame.shape[1] // 500)
+  try:
+    fps = 1000 / int(values["-FPS-"])
+  except:
+    fps = 1000 / 60
+  event, values = mainWindow.read(timeout=fps)
+  if playing and len(frames) > 0:
+    displayFrame = cv2.imread(f"frames/{currentFrame}.png")
+    imagebytes = cv2.imencode('.png', displayFrame)[1].tobytes()
+    mainWindow["-FRAME-"].update(data=imagebytes, subsample=displayFrame.shape[1] // 500)
+    currentFrame += 1
+    if (currentFrame == len(frames)):
+      currentFrame = 0
   match event:
+    case sg.WIN_CLOSED:
+      break
+    case "-PLAY-":
+      if playing:
+        playing = False
+      else:
+        playing = True
     case "-TAKEIMAGE-":
-      continue
+      cv2.imwrite(f"frames/{nextFrame}.png",frame)
+      frames.append(nextFrame)
+      mainWindow["-FRAMES-"].update(frames)
+      nextFrame += 1
+    case "-FRAMES-":
+      selected = values["-FRAMES-"][0]
+      displayFrame = cv2.imread(f"frames/{selected}.png")
+      imagebytes = cv2.imencode('.png', displayFrame)[1].tobytes()
+      mainWindow["-FRAME-"].update(data=imagebytes, subsample=displayFrame.shape[1] // 500)
 
 mainWindow.close()
